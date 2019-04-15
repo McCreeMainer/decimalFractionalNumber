0ba13765eda6b0fdd401c92f61ce35517b59055a
Лихолетов Михаил <odaving111@gmail.com>
2019-04-15 23:20:13 +0300

    ////////////////////PATCH NOTES////////////////////
    * Function "plusminus" added
    
    * Function "charToInt" added
    
    * Fixed big numbers in Double & Float
    
    * Fixed concatenation of Strings
    
    * Some small fixes

---
+++ src/dfn/DecimalFractionalNumber.java
@@ -0,0 +1,204 @@
+package dfn;
+
+public final class DecimalFractionalNumber {
+
+    private String number;
+    private int count;
+    private int pointPosition;
+
+    public DecimalFractionalNumber(int input, int c) {
+        count = c;
+        number = "" + input;
+        pointPosition = number.length();
+    }
+
+    public DecimalFractionalNumber(long input, int c) {
+        count = c;
+        number = "" + input;
+        pointPosition = number.length();
+    }
+
+    public DecimalFractionalNumber(float input, int c) {
+        count = c;
+        number = String.format("%." + c + "f", input).replace(",", ".");
+        if (Float.parseFloat(number) != input) throw new IllegalArgumentException();
+        roundNulls(number);
+    }
+
+    public DecimalFractionalNumber(double input, int c) {
+        count = c;
+        number = String.format("%." + c + "f", input).replace(",", ".");
+        if (Double.parseDouble(number) != input) throw new IllegalArgumentException();
+        roundNulls(number);
+    }
+
+    public DecimalFractionalNumber(String input, int c) {
+        count = c;
+        if (input.contains(".")) {
+            pointPosition = input.indexOf(".");
+            number = input.substring(0, pointPosition) + input.substring(pointPosition + 1);
+        }
+        else {
+            number = input;
+            pointPosition = number.length();
+        }
+    }
+
+    private void roundNulls(String a) {
+        for (int i = 0; i < a.length(); i++) if (a.charAt(i) == '.') {
+            pointPosition = i;
+            number = a.substring(0, pointPosition) + a.substring(pointPosition + 1);
+            break;
+        }
+        while (number.length() - 1 >= pointPosition && number.charAt(number.length() - 1) == '0')
+            number = number.substring(0, number.length() - 1);
+        if (number.length() > count) throw new IllegalArgumentException();
+    }
+
+    private DecimalFractionalNumber plusminus(DecimalFractionalNumber oth, boolean sign) {
+        String aBefore = number.substring(0, pointPosition);
+        String bBefore = oth.number.substring(0, oth.pointPosition);
+        String aAfter = number.substring(pointPosition);
+        String bAfter = oth.number.substring(oth.pointPosition);
+        int point = Math.max(aBefore.length(), bBefore.length());
+        String [] arr = new String[Math.max(aBefore.length(), bBefore.length())
+                + Math.max(aAfter.length(), bAfter.length()) + 1];
+        int nxt = 0;
+        for (int i = arr.length - 1; i >= 0; i--) {
+            int a = 0;
+            int b = 0;
+            int result = 0;
+            if (i == point) {
+                arr[i] = ".";
+                continue;
+            }
+            else if (i > point) {
+                if (point + aAfter.length() < i) a = 0;
+                else a = charToInt(aAfter.charAt(i - point - 1));
+                if (point + bAfter.length() < i) b = 0;
+                else b = charToInt(bAfter.charAt(i - point - 1));
+            }
+            else {
+                if (point - aBefore.length() > i) a = 0;
+                else a = charToInt(aBefore.charAt(i - point + aBefore.length()));
+                if (point - bBefore.length() > i) b = 0;
+                else b = charToInt(bBefore.charAt(i - point + bBefore.length()));
+            }
+            if (sign) {
+                result = (a + 10 - b) % 10;
+                arr[i] = String.valueOf((result + 10 - nxt) % 10);
+                if (a < b || result < nxt) nxt = 1;
+                else nxt = 0;
+            }
+            else {
+                result = a + b;
+                arr[i] = String.valueOf((result + nxt) % 10);
+                nxt = (result + nxt) / 10;
+            }
+        }
+        StringBuilder num = new StringBuilder();
+        for (String s : arr) num.append(s);
+        return new DecimalFractionalNumber(num.toString(), Math.max(count, oth.count));
+    }
+
+    public DecimalFractionalNumber plus(DecimalFractionalNumber other) {
+        return plusminus(other, false);
+    }
+
+    public DecimalFractionalNumber minus(DecimalFractionalNumber other) {
+        return plusminus(other, true);
+    }
+
+    public DecimalFractionalNumber multiplication(DecimalFractionalNumber other) {
+        String[] arr = new String[number.length() + other.number.length()];
+        for (int i = 0; i < number.length(); i++) {
+            int nxt = 0;
+            for (int j = 0; j < other.number.length(); j++) {
+                int ar = 0;
+                if (arr[arr.length - i - j - 1] != null) ar = Integer.parseInt(arr[arr.length - i - j - 1]);
+                int result = (number.charAt(number.length() - i - 1) - 48)
+                                * (other.number.charAt(other.number.length() - j - 1) - 48) + nxt + ar;
+                arr[arr.length - i - j - 1] = String.valueOf(result % 10);
+                nxt = result / 10;
+            }
+            int j = other.number.length();
+            while (nxt != 0) {
+                int ar = 0;
+                if (arr[arr.length - i - j - 1] != null) ar = Integer.parseInt(arr[arr.length - i - j - 1]);
+                arr[arr.length - i - j - 1] = String.valueOf(nxt % 10 + ar);
+                nxt /= 10;
+                j++;
+            }
+        }
+        int j = 0;
+        StringBuilder num = new StringBuilder();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == null) {
+                j++;
+                continue;
+            }
+            if (i == arr.length - (number.length() - pointPosition) - (other.number.length() - other.pointPosition))
+                num.append(".");
+            if (i - j >= Math.min(other.count, count) - 1) {
+                int nxt = 0;
+                for (int l = arr.length - 1; l > i; l--) {
+                    if (Integer.parseInt(arr[l]) + nxt >= 5) nxt = 1;
+                    else nxt = 0;
+                }
+                num.append(Integer.parseInt(arr[i]) + nxt);
+                break;
+            }
+            num.append(arr[i]);
+        }
+        return new DecimalFractionalNumber(num.toString(), Math.min(count, other.count));
+    }
+
+    public DecimalFractionalNumber roundOff(int scale) {
+        if (pointPosition > scale) throw new IllegalArgumentException();
+        int a = number.length();
+        if (scale < number.length()) a = scale;
+        return new DecimalFractionalNumber(number.substring(0, pointPosition) + "."
+                + number.substring(pointPosition, a), scale);
+    }
+
+    private int charToInt(char a) {
+        return (int) a - 48;
+    }
+
+    public int toInt() {
+        int f = Integer.parseInt(number.substring(0, pointPosition));
+        return f;
+    }
+
+    public long toLong() {
+        long f = Long.parseLong(number.substring(0, pointPosition));
+        return f;
+    }
+
+    public float toFloat() {
+        float f = Float.parseFloat(number.substring(0, pointPosition) + "." + number.substring(pointPosition));
+        return f;
+    }
+
+    public double toDouble() {
+        double f = Double.parseDouble(number.substring(0, pointPosition) + "." + number.substring(pointPosition));
+        return f;
+    }
+
+    @Override
+    public String toString() {
+        String result = number.substring(0, pointPosition);
+        if (pointPosition != number.length()) result += "." + number.substring(pointPosition);
+        return result;
+    }
+
+    @Override
+      public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj instanceof DecimalFractionalNumber) {
+            DecimalFractionalNumber other = (DecimalFractionalNumber) obj;
+            return number.equals(other.number) && count == other.count && pointPosition == other.pointPosition;
+        }
+        return false;
+    }
+}
--- test/dfn/DFNTest.java
+++ test/dfn/DFNTest.java
@@ -6,100 +6,99 @@ class DFNTest {
 
     @org.junit.jupiter.api.Test
     void plus() {
-        assertEquals(new decimalFractionalNumber(3.353, 4),
-                new decimalFractionalNumber(2.353, 4).plus(new decimalFractionalNumber(1.0, 2)));
-        assertEquals(new decimalFractionalNumber(3.353, 6),
-                new decimalFractionalNumber(2.353f, 6).plus(new decimalFractionalNumber(1, 1)));
-        assertEquals(new decimalFractionalNumber(3.0, 4),
-                new decimalFractionalNumber(2, 4).plus(new decimalFractionalNumber(1.0, 2)));
-        assertEquals(new decimalFractionalNumber(200.0, 4),
-                new decimalFractionalNumber(199l, 4).plus(new decimalFractionalNumber(1.0, 2)));
-        assertEquals(new decimalFractionalNumber(3.353, 4),
-                new decimalFractionalNumber("2.353", 4).plus(new decimalFractionalNumber(1.0, 2)));
+        assertEquals(new DecimalFractionalNumber(3.353, 4),
+                new DecimalFractionalNumber(2.353, 4).plus(new DecimalFractionalNumber(1.0, 2)));
+        assertEquals(new DecimalFractionalNumber(3.353, 6),
+                new DecimalFractionalNumber(2.353f, 6).plus(new DecimalFractionalNumber(1, 1)));
+        assertEquals(new DecimalFractionalNumber(3.0, 4),
+                new DecimalFractionalNumber(2, 4).plus(new DecimalFractionalNumber(1.0, 2)));
+        assertEquals(new DecimalFractionalNumber(200.0, 4),
+                new DecimalFractionalNumber(199l, 4).plus(new DecimalFractionalNumber(1.0, 2)));
+        assertEquals(new DecimalFractionalNumber(3.353, 4),
+                new DecimalFractionalNumber("2.353", 4).plus(new DecimalFractionalNumber(1.0, 2)));
     }
 
     @org.junit.jupiter.api.Test
     void minus() {
-        assertEquals(new decimalFractionalNumber(1.353, 4),
-                new decimalFractionalNumber(2.353, 4).minus(new decimalFractionalNumber(1.0, 2)));
-        assertEquals(new decimalFractionalNumber(1.353, 6),
-                new decimalFractionalNumber(2.353f, 6).minus(new decimalFractionalNumber(1, 1)));
-        assertEquals(new decimalFractionalNumber(1.0, 4),
-                new decimalFractionalNumber(2, 4).minus(new decimalFractionalNumber(1.0, 2)));
-        assertEquals(new decimalFractionalNumber(199.0, 4),
-                new decimalFractionalNumber(200l, 4).minus(new decimalFractionalNumber(1.0, 2)));
-        assertEquals(new decimalFractionalNumber(1.353, 4),
-                new decimalFractionalNumber("2.353", 4).minus(new decimalFractionalNumber(1.0, 2)));
-
-
+        assertEquals(new DecimalFractionalNumber(1.353, 4),
+                new DecimalFractionalNumber(2.353, 4).minus(new DecimalFractionalNumber(1.0, 2)));
+        assertEquals(new DecimalFractionalNumber(1.353, 6),
+                new DecimalFractionalNumber(2.353f, 6).minus(new DecimalFractionalNumber(1, 1)));
+        assertEquals(new DecimalFractionalNumber(1.0, 4),
+                new DecimalFractionalNumber(2, 4).minus(new DecimalFractionalNumber(1.0, 2)));
+        assertEquals(new DecimalFractionalNumber(199.0, 4),
+                new DecimalFractionalNumber(200l, 4).minus(new DecimalFractionalNumber(1.0, 2)));
+        assertEquals(new DecimalFractionalNumber(1.353, 4),
+                new DecimalFractionalNumber("2.353", 4).minus(new DecimalFractionalNumber(1.0, 2)));
     }
 
     @org.junit.jupiter.api.Test
     void multiplication() {
-        assertEquals(new decimalFractionalNumber(4.706, 4),
-                new decimalFractionalNumber(2.353, 4).multiplication(new decimalFractionalNumber(2.0, 2)));
-        assertEquals(new decimalFractionalNumber("0.0470", 5),
-                new decimalFractionalNumber(0.2353f, 5).multiplication(new decimalFractionalNumber(0.2, 2)));
-        assertEquals(new decimalFractionalNumber(56, 4),
-                new decimalFractionalNumber(28, 4).multiplication(new decimalFractionalNumber(2, 2)));
-        assertEquals(new decimalFractionalNumber(470.6, 4),
-                new decimalFractionalNumber(2.353, 4).multiplication(new decimalFractionalNumber(200l, 3)));
-        assertEquals(new decimalFractionalNumber("0.470", 4),
-                new decimalFractionalNumber("2.353", 4).multiplication(new decimalFractionalNumber("0.2", 2)));
+        assertEquals(new DecimalFractionalNumber(4.7, 2),
+                new DecimalFractionalNumber(2.353, 4).multiplication(new DecimalFractionalNumber(2.0, 2)));
+        assertEquals(new DecimalFractionalNumber(0.1, 2),
+                new DecimalFractionalNumber(0.2353f, 5).multiplication(new DecimalFractionalNumber(0.2, 2)));
+        assertEquals(new DecimalFractionalNumber(56, 2),
+                new DecimalFractionalNumber(28, 4).multiplication(new DecimalFractionalNumber(2, 2)));
+        assertEquals(new DecimalFractionalNumber(109, 3),
+                new DecimalFractionalNumber(0.470, 4).multiplication(new DecimalFractionalNumber(232l, 3)));
+        assertEquals(new DecimalFractionalNumber(0.5, 2),
+                new DecimalFractionalNumber("2.353", 4).multiplication(new DecimalFractionalNumber("0.2", 2)));
 
     }
 
     @org.junit.jupiter.api.Test
     void roundOff() {
-        assertEquals(new decimalFractionalNumber(2.35, 3), new decimalFractionalNumber(2.353, 4).roundOff(3));
-        assertEquals(new decimalFractionalNumber(2.353f, 6), new decimalFractionalNumber(2.353f, 4).roundOff(6));
-        assertEquals(new decimalFractionalNumber(1, 1), new decimalFractionalNumber(1, 4).roundOff(1));
-        assertEquals(new decimalFractionalNumber(200l, 3), new decimalFractionalNumber(200l, 73).roundOff(3));
-        assertEquals(new decimalFractionalNumber(2.353, 4), new decimalFractionalNumber("2.353234", 7).roundOff(4));
+        assertEquals(new DecimalFractionalNumber(2.35, 3), new DecimalFractionalNumber(2.353, 4).roundOff(3));
+        assertEquals(new DecimalFractionalNumber(2.353f, 6), new DecimalFractionalNumber(2.353f, 4).roundOff(6));
+        assertEquals(new DecimalFractionalNumber(1, 1), new DecimalFractionalNumber(1, 4).roundOff(1));
+        assertEquals(new DecimalFractionalNumber(200l, 3), new DecimalFractionalNumber(200l, 73).roundOff(3));
+        assertEquals(new DecimalFractionalNumber(2.353, 4), new DecimalFractionalNumber("2.353234", 7).roundOff(4));
     }
 
     @org.junit.jupiter.api.Test
     void toInt() {
-        assertEquals(2, new decimalFractionalNumber(2.353, 4).toInt());
-        assertEquals(1l, new decimalFractionalNumber(1.3f, 4).toInt());
-        assertEquals(1l, new decimalFractionalNumber(1, 4).toInt());
-        assertEquals(100l, new decimalFractionalNumber(100l, 4).toInt());
-        assertEquals(2l, new decimalFractionalNumber("2.353", 4).toInt());
+        assertEquals(2, new DecimalFractionalNumber(2.353, 4).toInt());
+        assertEquals(1l, new DecimalFractionalNumber(1.3f, 4).toInt());
+        assertEquals(1l, new DecimalFractionalNumber(1, 4).toInt());
+        assertEquals(100l, new DecimalFractionalNumber(100l, 4).toInt());
+        assertEquals(2l, new DecimalFractionalNumber("2.353", 4).toInt());
     }
 
     @org.junit.jupiter.api.Test
     void toLong() {
-        assertEquals(2l, new decimalFractionalNumber(2.353, 4).toLong());
-        assertEquals(1l, new decimalFractionalNumber(1.3f, 4).toLong());
-        assertEquals(1l, new decimalFractionalNumber(1, 4).toLong());
-        assertEquals(100l, new decimalFractionalNumber(100l, 4).toLong());
-        assertEquals(2l, new decimalFractionalNumber("2.353", 4).toLong());
+        assertEquals(2l, new DecimalFractionalNumber(2.353, 4).toLong());
+        assertEquals(1l, new DecimalFractionalNumber(1.3f, 4).toLong());
+        assertEquals(1l, new DecimalFractionalNumber(1, 4).toLong());
+        assertEquals(100l, new DecimalFractionalNumber(100l, 4).toLong());
+        assertEquals(2l, new DecimalFractionalNumber("2.353", 4).toLong());
     }
 
     @org.junit.jupiter.api.Test
     void toFloat() {
-        assertEquals(1.353f, new decimalFractionalNumber(1.353, 4).toFloat());
-        assertEquals(1.3f, new decimalFractionalNumber(1.3f, 4).toFloat());
-        assertEquals(1.0f, new decimalFractionalNumber(1, 4).toFloat());
-        assertEquals(100.0f, new decimalFractionalNumber(100l, 4).toFloat());
-        assertEquals(1.353f, new decimalFractionalNumber("1.353", 4).toFloat());
+        assertEquals(1.353f, new DecimalFractionalNumber(1.353, 4).toFloat());
+        assertEquals(1.3f, new DecimalFractionalNumber(1.3f, 4).toFloat());
+        assertEquals(1.0f, new DecimalFractionalNumber(1, 4).toFloat());
+        assertEquals(100.0f, new DecimalFractionalNumber(100l, 4).toFloat());
+        assertEquals(1.353f, new DecimalFractionalNumber("1.353", 4).toFloat());
     }
 
     @org.junit.jupiter.api.Test
     void toDouble() {
-        assertEquals(1.353, new decimalFractionalNumber(1.353, 4).toDouble());
-        assertEquals(1.3, new decimalFractionalNumber(1.3f, 4).toDouble());
-        assertEquals(1.0, new decimalFractionalNumber(1, 4).toDouble());
-        assertEquals(100.0, new decimalFractionalNumber(100l, 4).toDouble());
-        assertEquals(1.353, new decimalFractionalNumber("1.353", 4).toDouble());
+        assertEquals(1.353, new DecimalFractionalNumber(1.353, 4).toDouble());
+        assertEquals(99999999999999.0, new DecimalFractionalNumber(99999999999999.0, 25).toDouble());
+        assertEquals(1.3, new DecimalFractionalNumber(1.3f, 4).toDouble());
+        assertEquals(1.0, new DecimalFractionalNumber(1, 4).toDouble());
+        assertEquals(100.0, new DecimalFractionalNumber(100l, 4).toDouble());
+        assertEquals(1.353, new DecimalFractionalNumber("1.353", 4).toDouble());
     }
 
     @org.junit.jupiter.api.Test
     void toStr() {
-        assertEquals("1.353", new decimalFractionalNumber(1.353, 4).toString());
-        assertEquals("1.3", new decimalFractionalNumber(1.3f, 4).toString());
-        assertEquals("1", new decimalFractionalNumber(1, 4).toString());
-        assertEquals("100", new decimalFractionalNumber(100l, 4).toString());
-        assertEquals("1.353", new decimalFractionalNumber("1.353", 4).toString());
+        assertEquals("99999999999999", new DecimalFractionalNumber(99999999999999.0, 25).toString());
+        assertEquals("1.3", new DecimalFractionalNumber(1.3f, 4).toString());
+        assertEquals("1", new DecimalFractionalNumber(1, 4).toString());
+        assertEquals("100", new DecimalFractionalNumber(100l, 4).toString());
+        assertEquals("1.353", new DecimalFractionalNumber("1.353", 4).toString());
     }
 }
